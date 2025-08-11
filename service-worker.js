9f7a234723a65a325917dc7a5f3039afd656163e
palanis10 <125899010+palanis10@users.noreply.github.com>
2025-08-11 13:34:58 -0500

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+// Offline cache (bump version to force updates)
+const CACHE_NAME = 'conga-pwa-clean-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k)))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const network = fetch(e.request).then((res) => {
+        caches.open(CACHE_NAME).then((c) => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
